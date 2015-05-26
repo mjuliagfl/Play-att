8a78b31381c5f5eeb9432d4ff1091500f8335028
Maria Júlia Godoy <mjgfl@cin.ufpe.br>
2015-05-26 15:05:13 -0300

    commit de julia priorizar tasks

--- Play Att/ViewControllerSelectedTasks.m
+++ Play Att/ViewControllerSelectedTasks.m
@@ -37,13 +37,144 @@
     self.tableSelectedTasks.delegate = self;
     // Do any additional setup after loading the view.
     
+    UILongPressGestureRecognizer *longPress = [[UILongPressGestureRecognizer alloc] initWithTarget:self  action:@selector(longPressGestureRecognized:)];
     
+    [self.tableSelectedTasks addGestureRecognizer: longPress];
     
     
     
     
     
 }
+
+
+
+
+
+-(IBAction)longPressGestureRecognized:(id)sender{
+    
+    UILongPressGestureRecognizer *longPress = (UILongPressGestureRecognizer *) sender;
+    UIGestureRecognizerState pState = longPress.state;
+    
+    CGPoint location = [longPress locationInView:self.tableSelectedTasks];
+    NSIndexPath *indexPath = [self.tableSelectedTasks indexPathForRowAtPoint:location];
+    
+    static UIView *snapshot = nil;
+    static NSIndexPath *sourceIndex = nil;
+    
+    switch(pState){
+        case UIGestureRecognizerStateBegan:{
+            if(indexPath){
+                sourceIndex = indexPath;
+                UITableViewCell *cell = [self.tableSelectedTasks cellForRowAtIndexPath:indexPath];
+                snapshot = [self customSnapshotFromView:cell];
+                
+                __block CGPoint center = cell.center;
+                snapshot.center = center;
+                snapshot.alpha = 0.0;
+                [self.tableSelectedTasks addSubview:snapshot];
+                
+                
+                [UIView animateWithDuration:0.25 animations:^{
+                    
+                    
+                    center.y = location.y;
+                    snapshot.center = center;
+                    snapshot.transform = CGAffineTransformMakeScale(1.05, 1.05);
+                    snapshot.alpha = 0.98;
+                    
+                    
+                    cell.alpha = 0.0;
+                    
+                } completion:^(BOOL finished) {
+                    
+                    cell.hidden = YES;
+                    
+                }];
+                
+            }
+            break;
+        }
+        case UIGestureRecognizerStateChanged: {
+            CGPoint center = snapshot.center;
+            center.y = location.y;
+            snapshot.center = center;
+            
+            if(indexPath && ![indexPath isEqual:sourceIndex]){
+                [self.selectedTasksList exchangeObjectAtIndex:indexPath.row withObjectAtIndex:sourceIndex.row];
+                
+                
+                [self.tableSelectedTasks moveRowAtIndexPath:sourceIndex toIndexPath:indexPath];
+                
+                
+                sourceIndex = indexPath;
+                
+                
+                
+            }
+            break;
+        }
+            
+        default: {
+            // Clean up.
+            UITableViewCell *cell = [self.tableSelectedTasks cellForRowAtIndexPath:sourceIndex];
+            cell.hidden = NO;
+            cell.alpha = 0.0;
+            [UIView animateWithDuration:0.25 animations:^{
+                
+                snapshot.center = cell.center;
+                snapshot.transform = CGAffineTransformIdentity;
+                snapshot.alpha = 0.0;
+                
+                // Undo fade out.
+                cell.alpha = 1.0;
+                
+            } completion:^(BOOL finished) {
+                
+                sourceIndex = nil;
+                [snapshot removeFromSuperview];
+                snapshot = nil;
+                
+            }];
+            break;
+        }
+            
+            
+            
+    }
+    
+    
+}
+
+
+- (UIView *)customSnapshotFromView:(UIView *)inputView {
+    
+    // Make an image from the input view.
+    UIGraphicsBeginImageContextWithOptions(inputView.bounds.size, NO, 0);
+    [inputView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    // Create an image view.
+    UIView *snapshot = [[UIImageView alloc] initWithImage:image];
+    snapshot.layer.masksToBounds = NO;
+    snapshot.layer.cornerRadius = 0.0;
+    snapshot.layer.shadowOffset = CGSizeMake(-5.0, 0.0);
+    snapshot.layer.shadowRadius = 5.0;
+    snapshot.layer.shadowOpacity = 0.4;
+    
+    //    for(int i = 0; i < 10; i++){
+    //        //printa a galera
+    //        NSLog([objs objectAtIndex:i]);
+    //
+    //    }
+    //    NSLog(@"\n \n");
+    
+    return snapshot;
+}
+
+
+
 
 -(UITableViewCell(*) ) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
